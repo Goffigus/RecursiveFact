3472a20300970e8da9fc238b4b4a4ad78d9d5911
Goffigus <56737195+Goffigus@users.noreply.github.com>
2019-11-25 11:35:46 -0500

    Add project files.

---
+++ RecursiveFact/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace RecursiveFact
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int f = 1;
+
+            string input = Console.ReadLine();
+
+            f = Convert.ToInt32(input);
+
+            
+            Console.WriteLine("The Factorial is {0}", fact(f));
+
+        }
+
+        private static int fact(int f)
+        {
+            if (f == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return f * fact(f - 1);
+            }
+        }
+    }
+}
